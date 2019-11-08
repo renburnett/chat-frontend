1983be9c5ad716da319b5bf1f3aaef3a80a49a06
Krista Loven <krista.loven.87@gmail.com>
2019-11-08 13:25:30 -0800

    fetched users

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,9 +1,18 @@
 import React, { PureComponent } from 'react'
+import User from '../components/User'
 
 class Sidebar extends PureComponent {
+
+  displayUsers = (users) => {
+    return users.map( (user)=> {
+      return <User key={user.id} user={user} />
+    })
+  }
+// will refactor to pull from state
+
   render () {
     return (
-      <div className='mt-5' > Hello!!!</div>
+      <div className='mt-5'> {this.displayUsers(this.props.users)}</div>
     )
   }
 }
