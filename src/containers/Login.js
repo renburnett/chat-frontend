7f35374c4f57c0be977c2c662054ea4c5da64d2c
Krista Loven <krista.loven.87@gmail.com>
2019-11-09 21:12:30 -0800

    built out form and incorporated styling

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,11 +3,27 @@ import React, { PureComponent } from 'react'
 class Login extends PureComponent {
   render () {
     return (
-      <div>
-        <form onSubmit={this.props.handleLogIn}>
-          <input type='text' placeholder='name' />
-          <button type='submit'>Login</button>
-        </form>
+      <div className='card bg-light mt-3 shadow'>
+        <div className='card-header'>
+          <h4>Welcome to Chat! Please login:</h4>
+        </div>
+        <div className='card-body'>
+          <form onSubmit={this.props.handleLogIn}>
+            <div className='form-group form-row'>
+              <label for='username' className='col-sm-3 col-form-label-sm '>Your Username</label>
+              <div className='col'>
+                <input type='text' placeholder='enter your username' id='username' className='form-control shadow-sm' required />
+              </div>
+            </div>
+            <div className='fom-group form-row'>
+              <label for='email' className='col-sm-3 col-form-label-sm'>Your Email</label>
+              <div className='col'>
+                <input type='text' placeholder='enter your email' id='email' className='form-control mb-3 shadow-sm' required />
+              </div>
+            </div>
+            <button className='btn btn-outline-primary btn-block shadow-sm' type='submit'>Login</button>
+          </form>
+        </div>
       </div>
     )
   }
