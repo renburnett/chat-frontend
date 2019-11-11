8ea85f202efcbf88236fe0227d1ee3b605c0e979
ianhunterharold <ianhunterharold@gmail.com>
2019-11-11 14:44:38 -0800

    when clicked, conversation appears with messages

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,13 +1,21 @@
 import React, { PureComponent } from 'react'
 import MessageForm from './MessageForm'
+import Message from '../components/Message'
 import PrivacyHOC from '../HOC/PrivacyHOC'
 
 class ChatWindow extends PureComponent {
+
+  displayConversationMessages = () => {
+    return this.props.currentConversation.messages.map((message) => {
+      return <Message message={message}/>
+    })
+  }
+
   render () {
     return (
       <div className='col-9 chat-window'>
+        {this.displayConversationMessages()}
         <MessageForm />
-        ChatWindow
       </div>
     )
   }
