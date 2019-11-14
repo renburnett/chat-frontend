a0f06b068018998b90fd55c088a57836163f7c8d
Krista Loven <krista.loven.87@gmail.com>
2019-11-13 18:19:52 -0800

    update navbar links, moved classes

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -8,20 +8,13 @@ class Navbar extends PureComponent {
     if (this.props.loggedIn) {
       return (
         <>
-          <NavLink to='/edituser'>
-            <div className='nav-item nav-link active' 
-            >Edit Profile</div>
-          </NavLink>
-          <NavLink to='/login'>
-            <div className='nav-item nav-link active' onClick={this.props.handleLogout}>Logout</div>
-          </NavLink>
+          <NavLink to='/edituser' className='nav-item nav-link'>Edit Profile</NavLink>
+          <NavLink to='/login' className='nav-item nav-link' onClick={this.props.handleLogout}>Logout</NavLink>
         </>
       )
     } else {
       return (
-        <NavLink to='/login'>
-          <div className='nav-item nav-link active'>Login</div>
-        </NavLink>
+        <NavLink className='nav-item nav-link' to='/login'>Login</NavLink>
       )
     }
   }
@@ -29,8 +22,8 @@ class Navbar extends PureComponent {
   render () {
     return (
       <nav className='navbar navbar-dark bg-dark'>
-        <NavLink to='/'>
-          <h1 className='navbar-brand'>Chat</h1>
+        <NavLink className='navbar-brand' to='/'>
+          <h1>Chat</h1>
         </NavLink>
         {this.userIsLoggedIn()}
       </nav>
